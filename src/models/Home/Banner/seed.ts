e0af0942097c3e07b334702b430f470a354ad454
riyad899 <raditkhanyt@gmail.com>
2026-03-03 02:02:01 +0600

    fixed

--- src/models/Home/Banner/seed.ts
+++ src/models/Home/Banner/seed.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-import { connectDb, closeDb } from '../../../config/db';
+import { connectDb, closeDb } from '../../../db';
 
 const seedBanner = async () => {
     try {
